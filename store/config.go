6fb24de088827984daafaf09b42b4ec69a95675c
minglei.23 <minglei.23@hotmail.com>
2024-01-07 20:22:47 -0400

    test

--- store/config.go
+++ store/config.go
@@ -10,7 +10,7 @@ const (
 	MysqlDB       = "v0"
 
 	StripeKey     = "sk_test_51OFXw4Lvs8YNyX8smm0SPVJwTaxKg31XcafaF8zTPHzzhuDaGOkjvi374NnUdiHf7JMYowaVQ4nK1Y6ac8rhVsTl00UTn0o3Ty"
-	StripeWebhook = "whsec_nPhUZttnZGcR3FVs2lXr8AJZgw4wIS8W"
+	StripeWebhook = "we_1OW6PXLvs8YNyX8siEVZQaS9"
 
 	PayPalClientID = "Abo6knU842QjT-gTg-78UGN6jMVmg0Hq8EoF_z6NmNhdl7L7GstsmR_DJGWrHosjnYkiqq_Lb6DOP7PM"
 	PayPalSecret   = "EINltfCmahqE0HghbfXj7rrVCYtkchynqjqh8hLASJd4Q0Nr4CSy3xUKvRV3gMRe4b1mj4Sj456gETqZ"
